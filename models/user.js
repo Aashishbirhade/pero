9c812b8dbe4f53932b3254fa38617e0b6f49b1aa
Aashish Birhade <134684645+Aashishbirhade@users.noreply.github.com>
2025-04-11 17:29:26 +0530

    user.js

--- models/user.js
+++ models/user.js
@@ -24,7 +24,7 @@
 const mongoose = require('mongoose');
 
 // Connect without deprecated options
-mongoose.connect("mongodb+srv://aashishbirhade:Ab%409765229769@cluster0.1usbk5t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb://127.0.0.1:27017/vision");
 
 const gameResultSchema = new mongoose.Schema({
   gameName: { type: String, required: true },
